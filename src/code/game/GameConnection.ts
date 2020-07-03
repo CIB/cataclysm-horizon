78b06f3bbbdfebc7c5aebe74b3599495daa50209
Christian Bielert <cib123@googlemail.com>
2020-07-03 20:35:22 +0200

    Implemented dfhack connection.

--- src/code/game/GameConnection.ts
+++ src/code/game/GameConnection.ts
@@ -1,23 +1,36 @@
 import { createConnection } from 'net'
 import { Struct } from 'struct'
+import { load, Root, Message, Type } from 'protobufjs'
 
-export interface TileMessage {
-    position: [number, number, number]
-    visible: boolean
-    terrain: number
-}
+const WordStruct = new Struct<{ value: number }>().word16Sle('value')
 
-const client = createConnection(8091)
+const HeaderStruct = new Struct<{
+    magic: string
+    version: number
+    REQUEST_MAGIC: string
+    RESPONSE_MAGIC: string
+}>()
+    .chars('magic', 8)
+    .word32Sle('version')
+
+const RPCMessageHeaderStruct = new Struct<{ id: number; size: number }>()
+    .word16Sle('id')
+    .chars('padding', 2)
+    .word32Sle('size')
+
+console.log('nice')
+const client = createConnection(5000)
 client.on('connect', () => {
-    console.log('successfully connected to game instance')
-    const header = HeaderStruct.allocate()
-    const message = JSON.stringify({'type': 'get_overmap_tile'})
-    header.fields.length = message.length
-    client.write(header.buffer())
-    client.write(message)
-    listenerLoop();
+    console.log('we are connected to DFHack!')
+
+    HeaderStruct.allocate()
+    HeaderStruct.set('magic', 'DFHack?\n')
+    HeaderStruct.set('version', 1)
+    const buf = HeaderStruct.buffer()
+    client.write(buf)
 })
 
+let buffer = new Buffer('', 'hex')
 
 function chunkString(str: string, len: number) {
     const size = Math.ceil(str.length / len)
@@ -32,9 +45,6 @@ function chunkString(str: string, len: number) {
     return r
 }
 
-
-let buffer = new Buffer('', 'hex')
-let connected = true
 const readData = async (bytes: number): Promise<Buffer> => {
     return new Promise((resolve, reject) => {
         if (buffer.length >= bytes) {
@@ -49,6 +59,7 @@ const readData = async (bytes: number): Promise<Buffer> => {
             reject(err)
         }
         const listener = (data: Buffer) => {
+            console.log('data', chunkString(data.toString('hex'), 4))
             buffer = Buffer.concat([buffer, data])
 
             if (buffer.length >= bytes) {
@@ -64,20 +75,140 @@ const readData = async (bytes: number): Promise<Buffer> => {
     })
 }
 
+let main = async () => {
+    const handshakeLength = HeaderStruct.allocate().buffer().length
+    console.log('handshakelength', handshakeLength)
+    const buffer = await readData(handshakeLength)
+    HeaderStruct.setBuffer(buffer)
+    console.log('got', HeaderStruct.get('version'))
 
-const HeaderStruct = new Struct<{
-    length: number
-}>()
-    .word32Ule('length')
+    const messageHeader = RPCMessageHeaderStruct.allocate()
+    const coreProtocol = await new Promise<Root>((resolve, reject) =>
+        load('./dfhack/library/proto/CoreProtocol.proto', (err, root) => {
+            if (err) reject(err)
+            else resolve(root)
+        })
+    )
+    const remoteFortressReader = await new Promise<Root>((resolve, reject) =>
+        load(
+            './dfhack/plugins/proto/RemoteFortressReader.proto',
+            (err, root) => {
+                if (err) reject(err)
+                else resolve(root)
+            }
+        )
+    )
 
-const listenerLoop = async () => {
-    while (connected) {
-        const header = HeaderStruct.allocate()
-        header.setBuffer(await readData(header.buffer().length))
-        console.log('header', header.fields)
-        const data = await readData(header.fields.length)
-        // console.log('data', data.toString('utf8'))
-        const json = JSON.parse(data.toString('utf8'))
-        console.log('message!', json)
+    const CoreBindRequest = coreProtocol.lookupType('CoreBindRequest')
+    const CoreBindReply = coreProtocol.lookupType('CoreBindReply')
+    const EmptyMessage = coreProtocol.lookupType('EmptyMessage')
+    const StringMessage = coreProtocol.lookupType('StringMessage')
+    const MaterialList = remoteFortressReader.lookupType('MaterialList')
+    const BlockRequest = remoteFortressReader.lookupType('BlockRequest')
+    const BlockList = remoteFortressReader.lookupType('BlockList')
+
+    interface RPCDefinition {
+        assignedId: number | undefined;
+        method: string;
+        plugin: string | null;
+        input: Type;
+        output: Type;
     }
+    const rpcTable: {[rpcName: string]: RPCDefinition} = {
+        'RegisterRPC': {
+            assignedId: 0,
+            method: 'RegisterRPC',
+            plugin: null,
+            input: CoreBindRequest,
+            output: CoreBindReply
+        }
+    };
+
+
+    async function callRPC<R, T>(
+        method: string,
+        request: R
+    ): Promise<T> {
+        const req = request !== undefined ? request : {};
+        const inType = rpcTable[method].input
+        const outType = rpcTable[method].output
+        const id = rpcTable[method].assignedId
+        const errMsg = inType.verify(req)
+        if (errMsg) {
+            throw Error(errMsg)
+        }
+        const requestProto = inType.create(req)
+        const requestBuf = inType.encode(requestProto).finish()
+        messageHeader.set('id', id)
+        messageHeader.set('size', requestBuf.length)
+        console.log('length', requestBuf.length)
+        client.write(messageHeader.buffer())
+        client.write(requestBuf)
+
+        let replyHeader = await readData(
+            RPCMessageHeaderStruct.allocate().buffer().length
+        )
+        console.log('header', replyHeader.toString('ascii'))
+        let replyHeaderStruct = RPCMessageHeaderStruct
+        replyHeaderStruct.setBuffer(replyHeader)
+        console.log('id', replyHeaderStruct.get('id'))
+        console.log('got size', replyHeaderStruct.fields.size)
+
+        return outType.decode(await readData(replyHeaderStruct.fields.size)) as any
+    }
+
+
+    async function registerRPC(method: string, plugin: string | null, input: Type, output: Type) {
+        const reply = await callRPC<{method: string, plugin: string | null, inputMsg: string, outputMsg: string}, { assignedId: number }>(
+            'RegisterRPC',
+            {
+                method,
+                plugin,
+                inputMsg: input.fullName.substr(1),
+                outputMsg: output.fullName.substr(1)
+            }
+        )
+        console.log('reply', reply)
+
+        rpcTable[method] = {
+            assignedId: reply.assignedId,
+            method,
+            plugin,
+            input,
+            output
+        }
+    }
+
+    console.log('empty message', EmptyMessage.fullName.substr(1))
+
+    await registerRPC('GetDFVersion', null, EmptyMessage, StringMessage)
+    await registerRPC('GetBlockList', 'RemoteFortressReader', BlockRequest, BlockList)
+    await registerRPC('ResetMapHashes', 'RemoteFortressReader', EmptyMessage, EmptyMessage)
+    
+    const dfVersion = await callRPC<void, {value: string}>(
+        'GetDFVersion',
+        undefined
+    )
+    console.log('dfVersion', dfVersion.value);
+    const blockRequest = {
+        blocksNeeded: 20,
+        minX: 100,
+        maxX: 110,
+        minY: 80,
+        maxY: 100,
+        minZ: 0,
+        maxZ: 160,
+    }
+    const resetHashReply = await callRPC<void, void>(
+        'ResetMapHashes',
+        undefined
+    )
+    console.log('reset hash', resetHashReply);
+    const blockReply = await callRPC(
+        'GetBlockList',
+        blockRequest
+    )
+    console.log('block reply', blockReply);
 }
+
+main()
